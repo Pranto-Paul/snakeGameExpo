d8cf67df04ebdc395349264cbf893bff109e602a
pranto paul <prantopaul55555@gmail.com>
2026-04-07 14:36:45 +0600

    feat: implement Game component with gesture-based direction handling
    Detecting gesture moving right,left,up,down

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,32 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet, Text } from 'react-native';
 import { Colors } from '../styles/Colors';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 const Game = () => {
+  const handleGesture = (event: any) => {
+    const { translationX, translationY } = event.nativeEvent;
+
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        console.log('Right');
+      } else {
+        console.log('Left');
+      }
+    } else {
+      if (translationY > 0) {
+        console.log('Down');
+      } else {
+        console.log('Up');
+      }
+    }
+  };
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Game content goes here!</Text>
-    </SafeAreaView>
+    <PanGestureHandler onGestureEvent={handleGesture}>
+      <SafeAreaView style={styles.container}>
+        <Text>Game content goes here!</Text>
+      </SafeAreaView>
+    </PanGestureHandler>
   );
 };
 
